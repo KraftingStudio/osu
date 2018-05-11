195ff851484d2a48fd86e31e619e030086f16aca
Kraft_ <kraftmajnmail@gmail.com>
2018-05-11 10:17:06 +0200

    Importing project
    I've imported the whole project.

---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MouseManager : MonoBehaviour {
+
+    public KeyCode key;     //to make a mouse click press mouseButton or key
+    private Transform GFX;  //mouseGraphics
+	
+    void Awake()
+    {
+        GFX = transform.GetChild(0);
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+
+        //show coursor
+        Vector2 temp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        GFX.position = temp;
+
+        //register click
+        if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(key))
+        {
+            print("CLICK");
+        }
+	}
+}
